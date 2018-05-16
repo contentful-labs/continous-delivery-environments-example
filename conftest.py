f179d13a3d41d4f6ac2f3bea03952bef5f74f5e9
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-05-16 14:59:03 -0400

    Broke migration into seperate steps

--- conftest.py
+++ conftest.py
@@ -3,48 +3,22 @@ import pytest
 import os
 from myapp import create_app, create_contentful
 from dotenv import load_dotenv
-import contentful_management
-import time
-
-
 
 load_dotenv()
 
 SPACE_ID = os.getenv('SPACE_ID')
 DELIVERY_API_KEY = os.getenv('DELIVERY_API_KEY')
-MANGEMENT_API_KEY = os.getenv('MANGEMENT_API_KEY')
 TESTING_ENV = 'circle_testing'
-
-client = contentful_management.Client(MANGEMENT_API_KEY)
-
-try:
-    environment = client.environments(SPACE_ID).find(TESTING_ENV)
-    print("Deleting previous environment {}".format(TESTING_ENV))
-    environment.delete()
-except:
-    print("{} environment doesn't exist".format(TESTING_ENV))
-
-print("Creating new Enivronment: {}".format(TESTING_ENV))
-environment = client.environments(SPACE_ID).create(
-    TESTING_ENV,
-    {
-        'name': TESTING_ENV
-    }
-)
-
-environment.name = TESTING_ENV
-print("Sleeping to allow creation of {}".format(TESTING_ENV))
-time.sleep(5)
 
 
 @pytest.fixture
 def app():
-    app = create_app(SPACE_ID, DELIVERY_API_KEY,environment_id='circle_testing')
+    app = create_app(SPACE_ID, DELIVERY_API_KEY,environment_id=TESTING_ENV)
     app.debug = True
     return app
 
 
 @pytest.fixture
 def contentful_client():
-    contentful_client = create_contentful(SPACE_ID, DELIVERY_API_KEY,environment_id='circle_testing')
+    contentful_client = create_contentful(SPACE_ID, DELIVERY_API_KEY,environment_id=TESTING_ENV)
     return contentful_client
---
+++ migration_prep.py
@@ -0,0 +1,43 @@
+import contentful_management
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SPACE_ID = os.getenv('SPACE_ID')
+DELIVERY_API_KEY = os.getenv('DELIVERY_API_KEY')
+MANGEMENT_API_KEY = os.getenv('MANGEMENT_API_KEY')
+TESTING_ENV = 'circle_testing'
+
+client = contentful_management.Client(MANGEMENT_API_KEY)
+
+try:
+    environment = client.environments(SPACE_ID).find(TESTING_ENV)
+    print("Deleting previous environment {}".format(TESTING_ENV))
+    environment.delete()
+except:
+    print("{} environment doesn't exist".format(TESTING_ENV))
+
+print("Creating new Enivronment: {}".format(TESTING_ENV))
+environment = client.environments(SPACE_ID).create(
+    TESTING_ENV,
+    {
+        'name': TESTING_ENV
+    }
+)
+time.sleep(1)
+
+environment.name = TESTING_ENV
+print("Polling to detect creation of {}".format(TESTING_ENV))
+
+while True:
+    try:
+        environment = client.environments(SPACE_ID).find(TESTING_ENV)
+        break
+    except:
+        print("{} environment doesn't exist. Sleeping for 1 second".format(TESTING_ENV))
+        time.sleep(1)
+
+print("Complete")
+
