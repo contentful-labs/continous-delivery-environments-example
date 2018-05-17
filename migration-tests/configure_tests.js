e05a91626edb657b0add3f4cde12a2f5b4254e47
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-05-16 20:03:05 -0400

    Added version check

--- migration-tests/configure_tests.js
+++ migration-tests/configure_tests.js
@@ -1,28 +1,34 @@
 // Basic example: update existing content type
 module.exports = function (migration) {
-    const expected_version = '0.0.1'
+    const expected_version = '0.0.2'
     const upgrade_version = '0.0.2'
 
-    migration.transformEntries({
-        contentType: 'versionTracking',
-        from: ['version'],
-        to: ['version'],
-        transformEntryForLocale: function (fromFields, currentLocale) {
-            console.log(fromFields.version[currentLocale])
-            if (fromFields.version[currentLocale] == expected_version){
-                return { version: upgrade_version };
+    // Checks if we need to run migration code:
+    if (expected_version != upgrade_version){
 
-            }else{
-                return { version: "Triggering Migration unsuccessful" };
+        //Checks if we are able to perform this migration. If not the migration will cause the build to fail.
+        migration.transformEntries({
+            contentType: 'versionTracking',
+            from: ['version'],
+            to: ['version'],
+            transformEntryForLocale: function (fromFields, currentLocale) {
+                console.log(fromFields.version[currentLocale])
+                if (fromFields.version[currentLocale] == expected_version){
+                    return { version: upgrade_version };
+
+                }else{
+                    return { version: "Triggering Migration unsuccessful" };
+                }
+                console.log(fromFields.version[currentLocale])
             }
-            console.log(fromFields.version[currentLocale])
-        }
-    });
+        });
 
-    const post = migration.editContentType('post');
+        // Runs migration
+        const post = migration.editContentType('post');
 
-    post.createField('first_appearance')
-        .type('Symbol')
-        .name('First Appearance of the character')
-        .required(false);
+        post.createField('first_appearance')
+            .type('Symbol')
+            .name('First Appearance of the character')
+            .required(false);
+    }
 }
