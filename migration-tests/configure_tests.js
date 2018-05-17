5f977e30a2affc00893785bc285eefceea8403da
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-05-16 20:05:10 -0400

    Rearranged code

--- migration-tests/configure_tests.js
+++ migration-tests/configure_tests.js
@@ -3,25 +3,26 @@ module.exports = function (migration) {
     const expected_version = '0.0.2'
     const upgrade_version = '0.0.2'
 
-    // Checks if we need to run migration code:
-    if (expected_version != upgrade_version){
 
-        //Checks if we are able to perform this migration. If not the migration will cause the build to fail.
-        migration.transformEntries({
-            contentType: 'versionTracking',
-            from: ['version'],
-            to: ['version'],
-            transformEntryForLocale: function (fromFields, currentLocale) {
-                console.log(fromFields.version[currentLocale])
-                if (fromFields.version[currentLocale] == expected_version){
-                    return { version: upgrade_version };
+    //Checks if this code base is able to work with this content schema version. If not the migration CLI will cause the build to fail.
+    migration.transformEntries({
+        contentType: 'versionTracking',
+        from: ['version'],
+        to: ['version'],
+        transformEntryForLocale: function (fromFields, currentLocale) {
+            console.log(fromFields.version[currentLocale])
+            if (fromFields.version[currentLocale] == expected_version){
+                return { version: upgrade_version };
 
-                }else{
-                    return { version: "Triggering Migration unsuccessful" };
-                }
-                console.log(fromFields.version[currentLocale])
+            }else{
+                return { version: "Triggering Migration unsuccessful" };
             }
-        });
+            console.log(fromFields.version[currentLocale])
+        }
+    });
+
+    // Checks if we need to run any migration code:
+    if (expected_version != upgrade_version){
 
         // Runs migration
         const post = migration.editContentType('post');
