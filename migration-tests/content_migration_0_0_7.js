a4f9c5e987ba0c81df0a8389c7940cc5c9c456b5
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-05-25 13:46:09 -0400

    Added author field and bumped to 0.0.7

---
+++ migration-tests/content_migration_0_0_7.js
@@ -0,0 +1,26 @@
+const migration_expected_version = "0.0.6";
+const migration_upgrade_version = "0.0.7";
+
+function runMigration(migration) {
+    const post = migration.editContentType("post");
+    post
+        .createField("author")
+        .name("author")
+        .type("Symbol")
+        .required(false);
+    return;
+}
+
+function validateVersion(expected_version, upgrade_version) {
+    if (upgrade_version == migration_upgrade_version) {
+        if (expected_version == migration_expected_version) {
+            return true;
+        }
+    }
+    return false;
+}
+
+module.exports = {
+    runMigration,
+    validateVersion
+};
