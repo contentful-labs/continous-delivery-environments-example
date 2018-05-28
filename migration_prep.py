490e7d3c5cb9ff00665ab245634bc504b971faad
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-05-27 21:59:02 -0400

    Set testinng to be on branches

--- migration_prep.py
+++ migration_prep.py
@@ -1,4 +1,7 @@
 import contentful_management
+from contentful_management.resource import Link
+import json
+import sys
 import time
 import os
 from dotenv import load_dotenv
@@ -8,7 +11,7 @@ load_dotenv()
 SPACE_ID = os.getenv("SPACE_ID")
 DELIVERY_API_KEY = os.getenv("DELIVERY_API_KEY")
 MANGEMENT_API_KEY = os.getenv("MANGEMENT_API_KEY")
-TESTING_ENV = "circle_testing"
+TESTING_ENV = sys.argv[1]
 
 client = contentful_management.Client(MANGEMENT_API_KEY)
 
@@ -34,4 +37,19 @@ while True:
         print("{} environment doesn't exist. Sleeping for 1 second".format(TESTING_ENV))
         time.sleep(1)
 
-print("Complete")
+print("Complete creation of new environment.")
+print("Updating API Key to allow access.")
+
+
+new_environment_link = Link({
+    "sys": {
+        "type": "Link",
+        "linkType": "Environment",
+        "id": TESTING_ENV
+    }
+})
+
+api_keys = client.api_keys(SPACE_ID).all()
+for api_key in api_keys:
+    api_key.environments.append(new_environment_link)
+    api_key.save()
