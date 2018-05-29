640b7489d368146e186a2310a08255cfe50f028e
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-05-29 10:51:57 -0400

    Added better error handling

--- migration_prep.py
+++ migration_prep.py
@@ -24,7 +24,7 @@ except:
 
 print("Creating new Enivronment: {}".format(TESTING_ENV))
 environment = client.environments(SPACE_ID).create(TESTING_ENV, {"name": TESTING_ENV})
-time.sleep(1)
+time.sleep(3)
 
 environment.name = TESTING_ENV
 print("Polling to detect creation of {}".format(TESTING_ENV))
@@ -37,7 +37,7 @@ while True:
         print("{} environment doesn't exist. Sleeping for 1 second".format(TESTING_ENV))
         time.sleep(1)
 
-print("Complete creation of new environment.")
+print("Completed creation of new environment.")
 
 new_environment_link = Link(
     {"sys": {"type": "Link", "linkType": "Environment", "id": TESTING_ENV}}
@@ -50,3 +50,4 @@ for api_key in api_keys:
     api_key.environments.append(new_environment_link)
     print("Updating API Key - {} to allow access.".format(api_key.id))
     api_key.save()
+print("Completed API Key updates.")
