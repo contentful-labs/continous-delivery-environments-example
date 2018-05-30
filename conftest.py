98c3859d5dea57680affa889c6e777ed4213248a
Shy Ruparel <Shy@users.noreply.github.com>
2018-05-30 16:17:56 +0100

    Author feature and bumped to 0.0.7
    * Added author field and bumped to 0.0.7
    
    * Fixed typo
    
    * Add abstraction for env varibles
    
    * Set testing to be on branches
    
    * Updating config.
    
    * Config update
    
    * Added better error handling
    
    * Linted python

--- conftest.py
+++ conftest.py
@@ -11,16 +11,30 @@ DELIVERY_API_KEY = os.getenv("DELIVERY_API_KEY")
 TESTING_ENV = "circle_testing"
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--environment-id",
+        action="store",
+        default="circle_testing",
+        help="environment-id",
+    )
+
+
 @pytest.fixture
-def app():
-    app = create_app(SPACE_ID, DELIVERY_API_KEY, environment_id=TESTING_ENV)
+def get_env_id(request):
+    return request.config.getoption("--environment-id")
+
+
+@pytest.fixture
+def app(get_env_id):
+    app = create_app(SPACE_ID, DELIVERY_API_KEY, environment_id=get_env_id)
     app.debug = True
     return app
 
 
 @pytest.fixture
-def contentful_client():
+def contentful_client(get_env_id):
     contentful_client = create_contentful(
-        SPACE_ID, DELIVERY_API_KEY, environment_id=TESTING_ENV
+        SPACE_ID, DELIVERY_API_KEY, environment_id=get_env_id
     )
     return contentful_client
