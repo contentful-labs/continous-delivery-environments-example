123cef94d90d2fa5d3b7e183cc6ff2538c5af95e
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-06-11 18:23:34 +0200

    Remove extra field

---
+++ migration-tests/content_migration_0_0_10.js
@@ -0,0 +1,22 @@
+const migration_expected_version = "0.0.9";
+const migration_upgrade_version = "0.0.10";
+
+function runMigration(migration) {
+    const post = migration.editContentType("post");
+    post.deleteField("author");
+    return;
+}
+
+function validateVersion(expected_version, upgrade_version) {
+    if (upgrade_version == migration_upgrade_version) {
+        if (expected_version == migration_expected_version) {
+            return true;
+        }
+    }
+    return false;
+}
+
+module.exports = {
+    runMigration,
+    validateVersion
+};
