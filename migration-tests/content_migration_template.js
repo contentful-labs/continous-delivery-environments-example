4ab433abeee8cf0c4aba7a90388f381cef8cfa7d
Shy Ruparel <Shy@users.noreply.github.com>
2018-06-12 15:47:03 +0200

    Reset for Amsterdam (#11)
    * Reset for Amsterdam
    
    * Removed duplicate validation function
    
    * Test new Migration Code
    
    * Updated pytest
    
    * Fill all authors in as Stan Lee
    
    * tweak migration scripts
    
    * More tweaking
    
    * Bump down Version Number
    
    * reset tests

--- migration-tests/content_migration_template.js
+++ migration-tests/content_migration_template.js
@@ -1,20 +1,7 @@
-const migrationExpectedVersion = "<expected content version>";
-const migrationUpgradeVersion = "<expected upgrade version>";
-
 function runMigration(migration) {
-    console.log("Insert migration code here");
-}
-
-function validateVersion(expected_version, upgrade_version) {
-    if (upgrade_version == migration_upgrade_version) {
-        if (expected_version == migration_expected_version) {
-            return true;
-        }
-    }
-    return false;
+  console.log("Insert migration code here");
 }
 
 module.exports = {
-    runMigration,
-    validateVersion
+  runMigration
 };
