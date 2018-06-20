263c2cbd42edcdc9951b4b7f33fb29fcc9801c21
Shy Ruparel <Shyamalruparel1991@gmail.com>
2018-06-20 10:09:47 -0400

    Bumped up to v7

--- migration-tests/content_migration_0_0_6.js
+++ migration-tests/content_migration_0_0_6.js
@@ -1,6 +1,3 @@
-const migration_expected_version = "0.0.5";
-const migration_upgrade_version = "0.0.6";
-
 function runMigration(migration) {
   const post = migration.editContentType("post");
   post.deleteField("last_appearance");
