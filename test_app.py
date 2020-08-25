8a8079862e1896a79bc26f3fbea73fa70293964e
Frarlon Rodriguez <frarlon.rodriguez@realself.com>
2020-08-25 11:04:15 -0600

    test

--- test_app.py
+++ test_app.py
@@ -14,7 +14,7 @@ class TestApp:
 
     def test_content_type_post(self, contentful_client):
         """Test content model of a procedure"""
-        post_content_type = contentful_client.content_type("Procedure")
+        post_content_type = contentful_client.content_type("procedure")
         title = next(d for d in post_content_type.fields if d.id == "title")
         assert title.id == "title"
         assert title.type == "Symbol"
