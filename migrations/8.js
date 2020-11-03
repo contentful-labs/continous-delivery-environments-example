325949cb348a9de778ae505bf01d13522b8203ca
Shy <Shy@users.noreply.github.com>
2020-11-03 10:50:43 -0500

    Added 8 and 9

--- migrations/8.js
+++ migrations/8.js
@@ -2,4 +2,5 @@ module.exports = function runMigration(migration) {
   const post = migration.editContentType("post");
   post.deleteField("movies");
   return;
+  // Hi!
 };
--- migrations/9.js
+++ migrations/9.js
@@ -6,4 +6,5 @@ module.exports = function runMigration(migration) {
     .type("Symbol")
     .required(false);
   return;
+  //Hi Again
 };
